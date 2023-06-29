2e1fbc6153706fda944c56c19f0418cc463eced1
Yongjin An <yongjin5352@gmail.com>
2023-06-29 12:34:02 +0900

    Todolist 완성

--- src/App.js
+++ src/App.js
@@ -1,24 +1,81 @@
-import logo from './logo.svg';
+import {useRef, useState} from 'react';
 import './App.css';
+import styled from "styled-components";
+
+const FlexContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: ${(props) => props.justifyContent};
+  margin:30px;
+`;
+
+const TitleElement = styled.div`
+  font-size: 50px;
+  font-weight: bold;
+`;
+
+const CardElement = styled.div`
+  height: 150px;
+  width: 150px;
+  margin: 20px;
+  border: solid 3px green;
+  padding: 15px;
+  border-radius: 10px;
+  word-wrap: break-word;
+`
+
+const ListAdder = ({list, setList}) => {
+  const newid = useRef(0);
+  const [textinput,setTextinput] = useState('');
+
+  const buttonOnClickHandler = () => {
+    setList([...list, {id: newid.current++, content:textinput}]);
+  }
+  const inputOnChangeHandler = (event) => {
+    setTextinput(event.target.value);
+  }
+  
+  return (
+    <FlexContainer justifyContent='center'>
+      <input type='text' value={textinput} onChange={inputOnChangeHandler}></input>
+      <button onClick={buttonOnClickHandler}>추가하기</button>
+    </FlexContainer>
+  )
+};
+
+const Title = ({name}) => {
+  return (
+    <FlexContainer justifyContent='center'>
+      <TitleElement>{name}</TitleElement>
+    </FlexContainer>
+  )
+};
+
+const Card = ({content, id}) =>{
+  return (
+    <CardElement key={id}>{content}</CardElement>
+  )
+}
+
+const Cards = ({list}) => {
+  return (
+    <FlexContainer justifyContent='flex-start'>
+      {
+        list.map((item) => <Card content={item.content} id={item.id}/>)
+      }
+    </FlexContainer>
+  )
+};
 
 function App() {
+  const [todos, setTodos] = useState([]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <ListAdder list={todos} setList={setTodos}/>
+      <Title name='Todo List'/>
+      <Cards list={todos}/>
+    </>
+    
   );
 }
 
